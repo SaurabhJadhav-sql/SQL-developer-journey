af3072acd1fddd9eaba8bfb8eec7d6675cd24c2d
SaurabhJadhav-sql <saurabhjadhav57345@gmail.com>
2026-03-03 11:53:25 +0530

    Update join_queries.sql

--- joins/join_queries.sql
+++ joins/join_queries.sql
@@ -1,11 +1,25 @@
--- INNER JOIN example
+-- =========================
+-- INNER JOIN Example
+-- =========================
 SELECT e.name, d.department_name
 FROM employees e
 INNER JOIN departments d
 ON e.department_id = d.department_id;
 
--- LEFT JOIN example
+
+-- =========================
+-- LEFT JOIN Example
+-- =========================
 SELECT e.name, d.department_name
 FROM employees e
 LEFT JOIN departments d
+ON e.department_id = d.department_id;
+
+
+-- =========================
+-- RIGHT JOIN Example
+-- =========================
+SELECT e.name, d.department_name
+FROM employees e
+RIGHT JOIN departments d
 ON e.department_id = d.department_id;
