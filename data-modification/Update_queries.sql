f2e266f0d345746647dc8749b0a43336f4e46a12
SaurabhJadhav-sql <saurabhjadhav57345@gmail.com>
2026-03-08 15:40:11 +0530

    Update Update_queries.sql

--- data-modification/Update_queries.sql
+++ data-modification/Update_queries.sql
@@ -1,3 +1,28 @@
+-- Create Table
+
+CREATE TABLE supplier (
+    sup_no VARCHAR(5) PRIMARY KEY,
+    sup_name VARCHAR(50),
+    item_supplied VARCHAR(50),
+    item_price INT,
+    city VARCHAR(50)
+);
+
+-- Insert Data
+
+INSERT INTO supplier VALUES
+('S1','Suresh','Keyboard',400,'Hyderabad'),
+('S2','Kiran','Processor',8000,'Delhi'),
+('S3','Mohan','Mouse',350,'Delhi'),
+('S4','Ramesh','Processor',9000,'Bangalore'),
+('S5','Manish','Printer',6000,'Mumbai'),
+('S6','Srikanth','Processor',8500,'Chennai');
+
+-- Display table
+
+SELECT * FROM supplier;
+
+
 -- Update Queries Practice
 
 -- Increase the price of Keyboard by 200
